aa2d0ca34d0f4ece181b84cfce6ae33a9a468911
TMS Web Studio <tmswebstudio@gmail.com>
2026-02-12 04:38:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "examfocusls-79315075-85fe4",
+  "appId": "1:573955057021:web:d701b2771c60ab7678d814",
+  "apiKey": "AIzaSyCVVJJOuGJEmo-ele799NjL7v25TjQwzDs",
+  "authDomain": "examfocusls-79315075-85fe4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "573955057021"
+};
